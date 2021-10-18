49900f34a48b918eb137da6952478d3a292f8072
Insthync <ittipon.bay@gmail.com>
2021-10-18 20:41:33 +0700

    Update codes follow LiteNetLibManager changes

--- DarkRiftTransportFactory.cs
+++ DarkRiftTransportFactory.cs
@@ -2,8 +2,6 @@
 
 public class DarkRiftTransportFactory : BaseTransportFactory
 {
-    public override bool CanUseWithWebGL { get { return false; } }
-
     public override ITransport Build()
     {
         return new DarkRiftTransport();
